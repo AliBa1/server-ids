48ec0aab33e41ccd0e7f43249c97fdcee23e0dc0
Ali <89438079+AliBa1@users.noreply.github.com>
2025-03-06 15:03:03 -0800

    setup middleware

--- main.go
+++ main.go
@@ -4,16 +4,43 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
+func loggerMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		t := time.Now().Local()
+		url := r.URL.Path
+		protocol := r.Proto
+		log.Printf("%s: GET Request on %s using protocol: %s", t, url, protocol)
+		next(w, r)
+	}
+}
+
+var middleware = []func(http.HandlerFunc)http.HandlerFunc{
+	// middleware that will run from bottom to top
+	loggerMiddleware,
+}
+
+func welcomeHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "Welcome to the server\n")
+}
+
 func main() {
+	h := welcomeHandler
+	for _, m := range middleware{
+		h = m(h)
+	}
+
 	r := mux.NewRouter()
 	
-	r.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(w, "Welcome to the server\n")
-	})
+	// r.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+	// 	fmt.Fprint(w, "Welcome to the server\n")
+	// })
+	
+	r.HandleFunc("/", h)
 
 	r.HandleFunc("/welcome/{user}", func(w http.ResponseWriter, req *http.Request) {
 		vars := mux.Vars(req)
