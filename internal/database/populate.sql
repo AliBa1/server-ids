2a6b98f2615015bbd5cd155e9c672fc3e3981428
Ali <89438079+AliBa1@users.noreply.github.com>
2025-04-24 15:00:49 -0700

    remove some users

--- internal/database/populate.sql
+++ internal/database/populate.sql
@@ -2,23 +2,24 @@
 INSERT INTO users (username, password, role, last_login_date) VALUES
 ('funguy123', 'admin12345', 'admin', NULL),
 ('bossman', 'emp12345', 'employee', NULL),
-('grumpy', 'guest12345', 'guest', NULL),
-('jpearson', 'guest12345', 'guest', NULL),
-('fredrick5', 'guest12345', 'guest', NULL),
-('ballhoggary', 'emp12345', 'employee', NULL),
+-- ('grumpy', 'guest12345', 'guest', NULL),
+-- ('jpearson', 'guest12345', 'guest', NULL),
+-- ('fredrick5', 'guest12345', 'guest', NULL),
+-- ('ballhoggary', 'emp12345', 'employee', NULL),
 ('erick', 'admin12345', 'admin', NULL),
-('barrylarry', 'emp12345', 'employee', NULL),
-('twotthree', 'guest12345', 'guest', NULL),
-('yap', 'guest12345', 'guest', NULL),
-('boardman45', 'guest12345', 'guest', NULL),
+-- ('barrylarry', 'emp12345', 'employee', NULL),
+-- ('twotthree', 'guest12345', 'guest', NULL),
+-- ('yap', 'guest12345', 'guest', NULL),
+-- ('boardman45', 'guest12345', 'guest', NULL),
 ('1819twenty', 'emp12345', 'employee', NULL),
-('opi', 'guest12345', 'guest', NULL),
+-- ('opi', 'guest12345', 'guest', NULL),
 ('patrick', 'guest12345', 'guest', NULL),
-('fred111', 'guest12345', 'guest', NULL),
+-- ('fred111', 'guest12345', 'guest', NULL),
 ('secure21', 'guest12345', 'guest', NULL);
 
 -- Documents
 INSERT INTO documents (title, content, locked) VALUES
 ('Onboarding Document', 'Welcome to the company.', 1),
 ('First Doc Ever', 'Everyone can see this document. Your welcome!', 0),
-('Top Secret Case Study', 'All contents of this document should be kept a secret. Only admins should have this document. Do NOT shore with anyone!', 1);
+('Top Secret Case Study', 'All contents of this document should be kept a secret. Only admins should have this document. Do NOT shore with anyone!', 1),
+('Open This', 'From this page there is not anything a user could do to trigger a detector. Go to the users page to try out an attack.', 0);
